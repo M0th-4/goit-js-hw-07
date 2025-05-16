240ef6d6e4e3e5fa3d9770d9091cb4b42003271b
M0th-4 <qwas5624@gmail.com>
2025-05-16 16:42:30 +0300

    homeseven

--- js/task-3.js
+++ js/task-3.js
@@ -1,26 +1,6 @@
-class StringBuilder {
-  #value;
-  constructor(value) {
-    this.#value = value;
-  }
-  getValue() {
-    return this.#value;
-  }
-  padEnd(str) {
-    this.#value = this.#value + str;
-  }
-  padStart(str) {
-    this.#value = str + this.#value;
-  }
-  padBoth(str) {
-    this.#value = str + this.#value + str;
-  }
-}
-const builder = new StringBuilder('.');
-console.log(builder.getValue()); // "."
-builder.padStart('^');
-console.log(builder.getValue()); // "^."
-builder.padEnd('^');
-console.log(builder.getValue()); // "^.^"
-builder.padBoth('=');
-console.log(builder.getValue()); // "=^.^="
+const textInput = document.querySelector('input#name-input');
+const textOutput = document.querySelector('span#name-output');
+
+textInput.addEventListener("input", () => {
+    const trimmedValue = textInput.value.trim();
+    textOutput.textContent = trimmedValue === "" ? "Anonymous" : trimmedValue})
